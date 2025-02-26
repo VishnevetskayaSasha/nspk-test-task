8564ca2ea5fc64b8485c7741e3f05dcde523e54f
VishnevetskayaSasha <visneveckaya.a@gmail.com>
2025-02-27 01:45:14 +0300

    add full web version + search bar

---
+++ bancs.js
@@ -0,0 +1,76 @@
+const bancs = [
+  {
+    "id": 1,
+    "name": "Сбербанк",
+    "img": "./img/banks/sberbank.png",
+    "link": "http://www.sberbank.ru/ru/person"
+  },
+  {
+    "id": 2,
+    "name": "ВТБ",
+    "img": "./img/banks/vtb.png",
+    "link": "https://www.vtb.ru/"
+  },
+  {
+    "id": 3,
+    "name": "Почта Банк",
+    "img": "./img/banks/pochtabank.png",
+    "link": "https://www.pochtabank.ru/"
+  },
+  {
+    "id": 4,
+    "name": "Тинькофф",
+    "img": "./img/banks/tbank.png",
+    "link": "https://www.tbank.ru/"
+  },
+  {
+    "id": 5,
+    "name": "Открытие",
+    "img": "./img/banks/open.png",
+    "link": "https://www.open.ru/"
+  },
+  {
+    "id": 6,
+    "name": "Газпромбанк",
+    "img": "./img/banks/gazprombank.png",
+    "link": "https://www.gazprombank.ru/"
+  },
+  {
+    "id": 7,
+    "name": "Банк Россия",
+    "img": "./img/banks/abr.png",
+    "link": "https://abr.ru/"
+  },
+  {
+    "id": 8,
+    "name": "РНКБ",
+    "img": "./img/banks/rncb.png",
+    "link": "https://www.rncb.ru/"
+  },
+  {
+    "id": 9,
+    "name": "Русский стандарт",
+    "img": "./img/banks/rsb.png",
+    "link": "https://www.rsb.ru/"
+  },
+  {
+    "id": 10,
+    "name": "Альфа Банк",
+    "img": "./img/banks/alfabank.png",
+    "link": "https://alfabank.ru/"
+  },
+  {
+    "id": 11,
+    "name": "Россельхоз Банк",
+    "img": "./img/banks/rshb.png",
+    "link": "https://www.rshb.ru/"
+  },
+  {
+    "id": 12,
+    "name": "Московский Кредитный Банк",
+    "img": "./img/banks/mkb.png",
+    "link": "https://mkb.ru/"
+  },
+]
+
+export {bancs};
---
+++ index.js
@@ -0,0 +1,41 @@
+import { bancs } from "./bancs.js";
+
+const banksCards = document.querySelector(".banks__cards");
+const inputSearch = document.querySelector('.banks__search');
+const closeBnt = document.querySelector(".cross-icon");
+const errorMessage = document.querySelector(".text_error-message");
+
+banksCards.innerHTML = "";
+
+inputSearch.addEventListener("input", function() {
+  const query = this.value.toLowerCase();
+  const results = bancs.filter(item => item.name.toLowerCase().includes(query));
+  renderResults(results);
+  closeBnt.style.display = "inline";
+});
+
+function renderResults(results) {
+  banksCards.innerHTML = ""; 
+  if (results.length !== 0) {
+    results.forEach(result => {
+      errorMessage.style.display ="none";
+      banksCards.innerHTML += `
+      <li class="banks__item">
+        <a class="banks__link" href=${result.link} target="_blank">
+          <img class="banks__img" src="${result.img}" alt=${result.name}>
+        </a>
+      </li>
+    `
+    });
+  } else {
+    errorMessage.style.display ="block";
+  }
+}
+
+closeBnt.addEventListener("click", () => {
+  inputSearch.value = "";
+  renderResults(bancs);
+  closeBnt.style.display = "none"; 
+})
+
+renderResults(bancs);
